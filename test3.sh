30f9dfabec21e25c1ab0d5e53e288d611a2d60cc
Christian Tschenett <chtz@me.com>
2016-06-04 23:03:05 +0200

    cleanups

--- test3.sh
+++ test3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ruby wf_start_data.rb test3_wf.rb | ruby wf_start.rb | json_pp > state.json
-cat state.json
 cat state.json | ruby wf_signal_data.rb test3_wf.rb | ruby wf_signal.rb | json_pp > state2.json
-cat state2.json
 cat state2.json | ruby wf_signal_data.rb test3_wf.rb | ruby wf_signal.rb | json_pp > state3.json
 cat state3.json
