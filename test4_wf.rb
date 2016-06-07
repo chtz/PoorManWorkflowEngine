029a62d15fafaf52f024b2b26160f8782d45757f
Christian Tschenett <chtz@me.com>
2016-06-07 07:17:45 +0200

    adjusted sample

--- test4_wf.rb
+++ test4_wf.rb
@@ -1,31 +1,36 @@
 @definition = Workflow.define do 
   start_node  :start,   
-              :default_transition => :state_a
+              :default_transition => :fork
+
+  fork_node   :fork,
+	      :a_transition => :state_a,
+              :b_transition => :state_b
               
   state_node  :state_a,
-              :default_transition => :state_b,
+              :default_transition => :join,
               :enter_action => lambda { |token|
                 token["command"] = ["HTTPGET", "http://tschenett.ch"]
               },
               :leave_action => lambda { |token|
                 if  token["result"] =~ /\<title\>(.+?)\<\/title\>/m
-                  token.variables.delete "result"
-                  token["tschenett.title"] = $1
+                  token.parent["tschenett.title"] = $1
                 end
               }
               
   state_node :state_b,
-             :default_transition => :state_c,
+             :default_transition => :join,
              :enter_action => lambda { |token|
                token["command"] = ["HTTPGET", "http://furthermore.ch"]
              },
              :leave_action => lambda { |token|
                if  token["result"] =~ /\<title\>(.+?)\<\/title\>/m
-                 token.variables.delete "result"
-                 token["furthermore.title"] = $1
+                 token.parent["furthermore.title"] = $1
                end
              }
-                            
+                           
+  join_node  :join,
+             :default_transition => :state_c
+ 
   state_node :state_c,
              :default_transition => :state_d
               
